b7557703be58aaa429d2ec103bc442639f08b49a
Nathaniel Hamovitz <18648574+nhamovitz@users.noreply.github.com>
2023-03-10 15:17:09 -0800

    reading and displaying video

--- hello.py
+++ hello.py
@@ -4,9 +4,16 @@ import cv2
 
 cap = cv2.VideoCapture('.\\media\\vid1_WIN_20230310_14_20_03_Pro.mp4')
 
+length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+print(length)
+
+
+
 while (cap.isOpened()):
     ret, frame = cap.read()
-    print(frame)
+    # print(frame)
+    print(type(frame))
+    print(frame.shape)
     cv2.imshow('Frame', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
