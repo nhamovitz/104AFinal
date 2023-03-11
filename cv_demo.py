67d279c0403b73872b8c5159df6e7b84d6b55064
Nathaniel Hamovitz <18648574+nhamovitz@users.noreply.github.com>
2023-03-10 16:33:55 -0800

    pulling out frames working

--- cv_demo.py
+++ cv_demo.py
@@ -7,14 +7,19 @@ cap = cv2.VideoCapture('.\\media\\vid1_WIN_20230310_14_20_03_Pro.mp4')
 length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 print(length)
 
-
-
+frame_number = 0
 while (cap.isOpened()):
     ret, frame = cap.read()
-    # print(frame)
-    print(type(frame))
-    print(frame.shape)
+    # print(frame_number)
+    if frame_number in (103, 104, 105):
+        print(frame_number)
+        print(frame)
+
+    # print(type(frame))
+    # print(frame.shape)
     cv2.imshow('Frame', frame)
+
+    frame_number +=1
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
--- process.py
+++ process.py
@@ -5,12 +5,18 @@ def sparse(path: str, cut_proportion = 2):
     cap = cv2.VideoCapture(path)
 
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    width, height = cap.get(cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    sparse_vid = np.empty(
-        (frame_count // cut_proportion,
-        height, width, 3)
-    )
+    # these come as floats for some reason
+    # note: we want to round up, not round or truncate
+    # that's weird to me, but you get an off-by-one error with n = 3 if this is `int(frame_count // cut_proportion), and same with n = 10 and `round(frame_count / cut_proportion)`
+    keep_count = int(frame_count // cut_proportion + 1)
+    # print(frame_count, int(frame_count // cut_proportion), frame_count / cut_proportion, keep_count)
+    width = int(width)
+    height = int(height)
+    
+    sparse_vid = np.empty((keep_count, height, width, 3))
 
     kept_frames = []
     frame_number = 0
@@ -19,16 +25,33 @@ def sparse(path: str, cut_proportion = 2):
         _, frame = cap.read()
 
         if frame_number % cut_proportion == 0:
-            sparse_vid[frame_number, :, :, :] = frame
-            kept_frames.append(frame_number)
-
-        frame_number += 1
+            index = round(frame_number / cut_proportion)
+            sparse_vid[index, :, :, :] = frame
+            kept_frames.append(frame_number)            
 
         if frame_number == frame_count - 1:
+            _, frame = cap.read()
+            frame_number += 1
+            assert frame is None
             cap.release()
+            break
+
+        frame_number += 1
+    
         
     return sparse_vid, kept_frames
 
+if __name__ == '__main__':
+    demo = '.\\media\\vid1_WIN_20230310_14_20_03_Pro.mp4'
+
+    sparse_vid, kept = sparse(demo)
+    print("every 2", sparse_vid.shape, kept)
+
+    sparse_vid, kept = sparse(demo, 3)
+    print("every 3", sparse_vid.shape, kept)
+
+    sparse_vid, kept = sparse(demo, 10)
+    print("every 10", sparse_vid.shape, kept)
 
 
 
