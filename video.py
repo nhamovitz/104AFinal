67c4b88b3fd69e54b10b65f6a0e3916718573dfe
Nathaniel Hamovitz <18648574+nhamovitz@users.noreply.github.com>
2023-03-15 02:10:05 -0700

    it was the data type! / weird in-between commit

--- video.py
+++ video.py
@@ -2,9 +2,20 @@ import cv2
 import numpy as np
 
 class Video:
-    def __init__(self, frames):
-        # assert len(frames.shape) == 4
+    def __init__(self, frames, metadata_from = None):
+        assert len(frames.shape) == 4
+        assert frames.dtype == np.uint8
+        assert frames.shape[4 - 1] == 3
         self.frames = frames
+
+        if metadata_from:
+            if type(metadata_from) == str:
+                self.info = metadata_from
+            if type(metadata_from) == type(self):
+                self.frame_count = metadata_from.frame_count
+                self.frame_rate  = metadata_from.frame_rate
+                self.info        = metadata_from.info
+
 
     @classmethod
     def from_file(cls, path):
@@ -21,8 +32,7 @@ class Video:
 
         print(f"{frame_count=}, {width=}, {height=}, {frame_rate=}")
 
-        # frames = np.zeros((frame_count, height, width, 3))
-        frames = []
+        frames = np.zeros((frame_count, height, width, 3), dtype=np.uint8)
 
         i = 0
         while (cap.isOpened()):
@@ -30,15 +40,10 @@ class Video:
             if not ret: # Error handling kinda
                 print(f"Error reading frame of video {path}")
 
-            # frames[i, :, :, :] = frame
-            frames.append(frame)
-
-            # print(type(frame))
-            # print(frame.shape)
-            cv2.imshow("test", frame)           
-            if cv2.waitKey(28) & 0xFF == ord('q'):
-                break
-
+            frames[i, ...] = frame
+            # cv2.imshow("test", frame)           
+            # if cv2.waitKey(28) & 0xFF == ord('q'):
+            #     break
 
             if i == frame_count - 1:
                 # we're done
@@ -47,8 +52,6 @@ class Video:
                 assert frame is None
                 cap.release()
             i += 1
-        cv2.destroyAllWindows()
-
 
         vid = cls(frames)
         vid.frame_count = frame_count
@@ -62,13 +65,43 @@ class Video:
         frame_delay = int(1000 // self.frame_rate)
 
         for frame in self.frames:
-            # print(type(frame))
-            # print(frame.shape)
             cv2.imshow(self.info, frame)
-
             if cv2.waitKey(frame_delay) & 0xFF == ord('q'):
                 break
+    
         cv2.destroyAllWindows()
+
+    @classmethod
+    def abs_error(cls, vid1, vid2):
+        return np.abs(vid1 - vid2)
+
+    @classmethod
+    def rel_error(cls, vid1, vid2):
+        return cls.abs_error(vid1, vid2) / 255
+
+
+def analyze_against(vid: Video, func, description: str, play_variant = True):
+    frames = vid.frames.copy()
+    new_frames = func(frames)
+    new_vid = Video(new_frames, metadata_from = vid)
+    if description:
+        if new_vid.info:
+            new_vid.info += description
+        else:
+            new_vid.info = description
+
+    if play_variant:
+        new_vid.play_video()
+
+    print(f"Error analysis for variant {description}.")
+    print(f"Mean absolute error: {np.mean(Video.abs_error(vid, new_vid))}")
+    rel_error = Video.rel_error(vid, new_vid)
+    print(f"Mean relative error: {np.mean(rel_error)}")
+    if play_variant:
+        rel_error = Video(rel_error)
+        rel_error.play_video
+
+
 
 
     
@@ -80,6 +113,38 @@ if __name__ == '__main__':
     demo_path = Path('.') / 'media' / 'vid1_WIN_20230310_14_20_03_Pro.mp4'
     vid = Video.from_file(str(demo_path))
     vid.play_video()
+
+
+    def flip_on(axis):
+        def f(frames):
+            return np.flip(frames, axis).copy()
+        return f
+
+    analyze_against(vid, flip_on(0), "Reverse")
+    analyze_against(vid, flip_on(1), "upside_down")
+    analyze_against(vid, flip_on(2), "sideways")
+    analyze_against(vid, lambda frames: np.roll(frames, 1, 3).copy(), "colors")
+
+    reverse = flip_on(frames, 0)
+    upside_down = flip_on(frames, 1)
+    sideways = flip_on(frames, 2)
+    colors = flip_on(frames, 3)
+
+    reverse = Video(reverse, vid)
+    upside_down = Video(upside_down, vid)
+    sideways = Video(sideways, vid)
+    colors = Video(colors, vid)
+
+    reverse.play_video()
+    upside_down.play_video()
+    sideways.play_video()
+    colors.play_video()
+
+    print("Error analysis (absolute only)")
+    print(f"Reverse: {np.mean(Video.abs_error(vid, reverse))}")
+    print(f"upside_down: {np.mean(Video.abs_error(vid, upside_down))}")
+    print(f"sideways: {np.mean(Video.abs_error(vid, sideways))}")
+    print(f"upside_down: {np.mean(Video.abs_error(vid, upside_down))}")
 
 
         
