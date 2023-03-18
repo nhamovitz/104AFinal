12371da97314a4d355e6cd2813c8eafaad5d7f0f
R.Alers <59209735+RAlers@users.noreply.github.com>
2023-03-18 00:09:33 -0700

    Added plotting feature for individual pixel

---
+++ numpy_method_plots.py
@@ -0,0 +1,58 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+from pathlib import Path
+
+from read_numpy_array_files import read_wonky_file
+
+def plot_a_pixel(frames_array, row, column, title="Plot of Spline Interpolation"):
+    """
+    Input: array of video we are analyzing, row of pixel, column of pixel, title of graph (default to spline)
+    Output: Labeled plot of red, green, and blue interpolations
+    """
+
+    x_vals = [i for i in range(len(frames_array))]
+    
+    red = np.zeros(len(frames_array))
+    green = np.zeros(len(frames_array))
+    blue = np.zeros(len(frames_array))
+
+    for f in x_vals:
+        red[f] = frames_array[f][row][column][0]
+        green[f] = frames_array[f][row][column][1]
+        blue[f] = frames_array[f][row][column][2]
+    
+    plt.plot(x_vals, red, color = 'red')
+    plt.plot(x_vals, green, color = 'green')
+    plt.plot(x_vals, blue, color = 'blue')
+    plt.xlabel("Frame Number")
+    plt.ylabel("RGB value (0 to 255)")
+    plt.title(title)
+    plt.legend(["Red Interpolation", "Green Interpolation", "Blue Interpolatoin"])
+
+    plt.show()
+
+def file_to_plot(npy_file, x_frac=0.5, y_frac=0.5, method="spline"):
+    """
+    Input: x fraction and y fraction of point along image
+    """
+    arr = read_wonky_file(npy_file)
+
+    row = int(y_frac*len(arr[0]))
+    column = int(x_frac*len(arr[0][0]))
+
+    header = "Plot of the " + method + \
+            " interpolation at the pixel at row " + str(row) + " and column " + str(column)
+
+    plot_a_pixel(arr, row=row, column=column, title=header)
+
+    return None
+
+if __name__ == "__main__":
+    # Spline Plot
+    path = str(Path('.') / ('keys_' + 'spline' + '_n=4.npy'))
+    file_to_plot(path)
+
+
