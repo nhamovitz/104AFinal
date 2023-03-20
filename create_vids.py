fixed underflow in calculating abs_error

create_vids.py
@@ -36,11 +36,11 @@ def sun():
     yellow = [60, 248, 250]
     green = [29, 135, 34]
 
-    frames = np.array(
+    small_frames = np.array(
         [
          [
           [light_blue]*5,
-          [yellow, light_blue,light_blue,light_blue, yellow,],
+          [yellow, light_blue, light_blue, light_blue, light_blue,],
           [green] * 5
          ],
          [
@@ -83,12 +83,34 @@ def sun():
     frames[4, 100:200, 400:500, :] = np.broadcast_to(yellow, (1, 100, 100, 3))
     
     # assert frames.shape == ((5, 3, 5, 3))
-    return frames
+    return (small_frames, frames)
+
+def investigating_error_vis():
+    minisun = sun()[0]
+    flipped = np.flip(minisun, 1)
+    minisun = minisun[1]
+    flipped = flipped[1]
+    # abs_error = np.abs(
+    #         np.subtract(minisun, flipped, dtype = np.int16),
+    #         , casting = 'unsafe'
+    #     )
+    diff = np.subtract(
+                minisun.astype(np.int16), flipped.astype(np.int16)
+            )
+    abs_error = np.abs(diff,)
+    print(abs_error.shape)
+    abs_error = abs_error.astype(np.uint8)
+    print(abs_error.shape)
+    print(minisun)
+    print(flipped)
+    print(diff)
+    print(abs_error)
 
 if __name__ == '__main__':
+    investigating_error_vis()
+
     # turns out opencv reads each pixel as
     # b, g, r
-
     red = np.array([255, 0, 0], dtype=np.uint8)
     red = np.tile(red, 400**2).reshape((400, 400, 3))
     while True:

video.py
@@ -83,7 +83,12 @@ class Video:
     @classmethod
     def abs_error(cls, vid1, vid2):
         # assert vid1.shape == vid2.shape
-        return np.abs(vid1.frames - vid2.frames)
+        err = np.abs(
+            np.subtract(
+                vid1.frames.astype(np.int16), vid2.frames.astype(np.int16)
+            ),
+        )
+        return err.astype(np.uint8)
 
     @classmethod
     def rel_error(cls, vid1, vid2):
@@ -124,7 +129,6 @@ def analyze_against(vid: Video, func, description: str, play_variant = True):
 
     print(f"Error analysis for variant {description}.")
     abs_error = Video.abs_error(vid, new_vid)
-    print(frames, new_frames, abs_error)
     print(f"Mean absolute error: {np.mean(abs_error)}")
     print(f"Mean relative error: {np.mean(Video.rel_error(vid, new_vid))}")
     if play_variant:
@@ -143,10 +147,10 @@ def demo1(vid):
             return np.flip(frames, axis).copy()
         return f
 
-    # analyze_against(vid, flip_on(0), "Reverse")
+    analyze_against(vid, flip_on(0), "Reverse")
     analyze_against(vid, flip_on(1), "upside_down")
-    # analyze_against(vid, flip_on(2), "sideways")
-    # analyze_against(vid, lambda frames: np.roll(frames, 1, 3).copy(), "colors")
+    analyze_against(vid, flip_on(2), "sideways")
+    analyze_against(vid, lambda frames: np.roll(frames, 1, 3).copy(), "colors")
     analyze_against(vid, lambda frames: np.zeros_like(frames), "black")
 
 
@@ -160,20 +164,27 @@ if __name__ == '__main__':
     import create_vids
 
 
-    demo_path = Path('.') / 'media' / 'sun.mp4'
-    # vid = Video.from_file(str(demo_path))
+    demo_path = Path('.') / 'media' / 'keys.mp4'
+    vid = Video.from_file(str(demo_path))
     # vid.frame_rate *= 0.7
-    vid = Video(create_vids.sun(), "`sun`")
-    vid.frame_rate = 1.1
+    # mini_sun, sun = create_vids.sun()
+    # vid = Video(sun, "`sun`")
+    # vid.frame_rate = 1.1
     # print(vid.frames)
     # vid.play_video()
-    while True:
-        demo1(vid)
-    exit()
-    lagrange = read_numpy_array_files.read_wonky_file(str(Path('.') / 'numpy_vids' /'sun_lagrange_neville_n=10.npy'))
-    lagrange = Video(lagrange, "lagrange")
-    lagrange.frame_rate = 4
-    lagrange.play_video()
+
+    # print(mini_sun)
+    # print(np.flip(mini_sun, 1))
+    # print(np.abs(mini_sun - np.flip(mini_sun, 1)))
+
+    # while True:
+    #     demo1(vid)
+    # exit()
+
+    # lagrange = read_numpy_array_files.read_wonky_file(str(Path('.') / 'numpy_vids' /'sun_lagrange_neville_n=10.npy'))
+    # lagrange = Video(lagrange, "lagrange")
+    # lagrange.frame_rate = 4
+    # lagrange.play_video()
     
     sparse, _ = run_demo()
     # sparse = create_vids.simple()